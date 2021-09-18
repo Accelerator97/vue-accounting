212e1a251c968b97bef85c2698c9969ac9aeafa3
薛斌 <928691436@qq.com>
2021-09-18 22:56:02 +0800

    封装icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,23 @@
 <template>
-     <div id="nav">
-      <router-link to="/money">记账</router-link> |
-      <router-link to="/label">标签</router-link> |
-       <router-link to="/statistics">统计</router-link>
-    </div>
+  <nav>
+    <router-link to="/money" class="item"> <Icon name="accounting" />记账</router-link>
+    | <router-link to="/label" class="item"> <Icon name="label" />标签</router-link> |
+    <router-link to="/statistics" class="item"> <Icon name="statistics" />统计</router-link>
+  </nav>
 </template>
 
-<script>
-    export default {
-        name:'nav'
-    }
+<script lang="ts">
+export default {
+  name: "Nav",
+};
 </script>
 
 <style lang="scss" scoped>
+nav{
+  display: flex;
+  box-shadow: ;
+  >.item{
 
+  }
+}
 </style>
