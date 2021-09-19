c46c4af8b2728a131d9dbcc0e6d8aa293dc81daf
薛斌 <928691436@qq.com>
2021-09-20 01:58:20 +0800

    完成Types组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注" />
+      </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Notes"
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: block;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    background: transparent;
+    border: none;
+    flex-grow: 1;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+      <ul class="types">
+        <li :class=" type === '-' && 'selected'" @click="selectType('-')">支出</li>
+        <li :class=" type === '+' && 'selected'" @click="selectType('+')">收入</li>
+      </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Component,Prop} from 'vue-property-decorator';
+@Component
+export default  class Types extends Vue{
+  type ='-';// '-'表示支出，'+'表示收入
+  @Prop(Number) xxx:number | undefined
+  //Number告诉Vue xxx运行时是一个Number
+  //Prop告诉Vue xxx是prop 不是data
+  //xxx 是属性名
+  // number | undefined 就是xxx的类型，告诉TS的xxx编译时的类型
+  selectType(type:string){
+    if(type !== '-' && type !== '+'){
+      throw new Error('type is unknown')
+    }
+    this.type = type 
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    line-height: 64px;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
