8bcc5a51c991c321bcd09b5e6f5b33a496d40415
薛斌 <928691436@qq.com>
2021-09-22 04:54:10 +0800

    两个小bug

--- src/components/FormItem.vue
+++ src/components/FormItem.vue
@@ -2,7 +2,12 @@
   <div>
     <label class="notes">
       <span class="name">{{ this.fieldName }}</span>
-      <input type="text" :value="value" @input="onValueChanged($event.target.value)" :placeholder="placeholder" />
+      <input
+        type="text"
+        :value="value"
+        @input="onValueChanged($event.target.value)"
+        :placeholder="placeholder"
+      />
     </label>
   </div>
 </template>
@@ -12,13 +17,9 @@ import Vue from "vue";
 import { Component, Prop, Watch } from "vue-property-decorator";
 @Component
 export default class FormItem extends Vue {
-@Prop({default:''}) value!:string
+  @Prop({ default: "" }) value!: string;
   @Prop({ required: true }) fieldName!: string;
   @Prop() placeholder?: string;
-  @Watch("value")
-  onValueChanged(value: string) {
-    this.$emit("update:value", value);
-  }
 }
 </script>
 
