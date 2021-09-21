546b2545795152d4e177e49a5d5892e4f84a8912
薛斌 <928691436@qq.com>
2021-09-22 02:48:40 +0800

    创造Button组件，完成EditLabel样式

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+     <button class="button" @click="$emit('click',$event)"><slot/></button>
+     <!-- 点击的是button,怎么触发Button上的click事件，就是在button上通过emit将点击的event事件传递出去-->
+     <!-- 或者<Button class="createTag" @click.native="createTag">新建标签</Button> -->
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    @Component
+    export default class Button extends Vue {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.button {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-Wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
+</style>
---
+++ src/components/FormItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">{{ this.fieldName }}</span>
+      <input type="text" v-model="value" :placeholder="placeholder" />
+    </label>
+  </div>
+</template>
+
+<script lang="ts" >
+import Vue from "vue";
+import { Component, Prop, Watch } from "vue-property-decorator";
+@Component
+export default class FormItem extends Vue {
+  value = "";
+  @Prop({ required: true }) fieldName!: string;
+  @Prop() placeholder?: string;
+  @Watch("value")
+  onValueChanged(value: string) {
+    this.$emit("update:value", value);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+.notes {
+  font-size: 14px;
+  display: block;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 40px;
+    background: transparent;
+    border: none;
+    flex-grow: 1;
+    padding-right: 16px;
+  }
+}
+</style>
